fc0238fab7f78e88fc1f18e3f339aa26b24f79df
KiralyTamas <tomaszpest@gmail.com>
2021-11-05 11:53:45 +0100

    Előrelépés történt, hamarosan kész.

--- json_for_converting/match/main.py
+++ json_for_converting/match/main.py
@@ -10,3 +10,15 @@ source = open(path, "r",encoding="utf-8")
 data = json.load(source)
 source.close()
 csv_path = path.replace(old, new)
+
+def convert_csv():
+  u=[]
+  for a in data:
+    u.append(a)
+  f=iter(data)
+  for i in f:
+    print(data[i])
+    x=iter(data[i])
+    print(u)
+
+convert_csv()
