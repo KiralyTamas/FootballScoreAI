9cd353b06151d832db68c9426c6f4ae566ea6bd1
Király Tamás <tomaszpest@gmail.com>
2021-11-06 11:04:48 +0100

    Végre, kész a match fájlok konvertálása is.

--- json_for_converting/match/main.py
+++ json_for_converting/match/main.py
@@ -1,21 +1,22 @@
 import csv
 import json
+from pathlib import Path
 
-old = "json"
-new = "csv"
+path=input("Mi a konvertálandó fájl útvonala: ")
+file_name=Path(path).name
+source = open(file_name, "r",encoding="utf-8")
 data = []
-path="match16000.json"
-#path=input("Mi a konvertálandó fájl útvonala: ")
-source = open(path, "r",encoding="utf-8")
 data = json.load(source)
 source.close()
-csv_path = path.replace(old, new)
+file_xxx=['json','csv']
+csv_path = file_name.replace(file_xxx[0],file_xxx[1])
 
 def convert_csv():
   with open(csv_path,"w",encoding="UTF-8",newline='') as file:
     csv_file=csv.writer(file)
+    id_header=data["h"][0]
+    csv_file.writerow(id_header)
     for ids in data.keys():
-        csv_file.writerow(data[ids][0])
         for value in range(len(data[ids])):
           csv_file.writerow(data[ids][value].values())
 
