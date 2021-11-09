4bf63f137cb3cefeb4cb655db80e1ffc82cf7b19
KiralyTamas <tomaszpest@gmail.com>
2021-11-09 16:26:27 +0100

    Fejlesztve és fájl átnevezve.

---
+++ Convert2_0.py
@@ -0,0 +1,28 @@
+import os
+from datas_for_converting.file_caller import caller
+from datas_for_converting.fixtures_result_converter import convert_fr
+from datas_for_converting.match_converter import convert_m
+from datas_for_converting.table_converter import convert_t
+
+f = []
+for (dirpath, dirnames, filenames) in os.walk(input("Mi a konvertálandó fájl útvonala: ")):
+    f.extend(filenames)
+    break
+
+
+def convert():
+  main_path=f
+  for i in main_path:
+    all_path=str(dirpath)+"\\"+str(i)
+    recall= caller(all_path)
+    if recall[0] == all_path[-4:]:
+      continue
+    if "fixtures" in recall[0]:
+      convert_fr(recall,"_")
+    if "result" in recall[0]:
+      convert_fr(recall,"_")
+    if "match" in recall[0]:
+      convert_m(recall)
+    if "table" in recall[0]:
+      convert_t(recall)
+convert()
--- datas_for_converting/file_caller.py
+++ datas_for_converting/file_caller.py
@@ -3,11 +3,16 @@ import os
 
 
 def caller(path):
-    relative_path = os.path.relpath(path)
-    source = open(relative_path, "r")
-    data = json.load(source)
-    file_xxx = ["json", "csv"]
-    csv_path = relative_path.replace(file_xxx[0], file_xxx[1])
-    source.close()
-    data_list = [csv_path, data]
-    return data_list
+    last_four=path[-4:]
+    if last_four == ".csv":
+        data_list=[last_four,"_"]
+        return data_list
+    else:
+        relative_path = os.path.relpath(path)
+        source = open(relative_path, "r")
+        data = json.load(source)
+        file_xxx = ["json", "csv"]
+        csv_path = relative_path.replace(file_xxx[0], file_xxx[1])
+        source.close()
+        data_list = [csv_path, data]
+        return data_list
