2f298589e559a22d80d56e33d97834bd51c3dbe3
KiralyTamas <tomaszpest@gmail.com>
2021-11-16 13:35:14 +0100

    jegyzet az otthoni munkához.

--- TERV.cmd
+++ TERV.cmd
@@ -1,3 +1,11 @@
 Sikerült megcsinálni, hogy a fixtures-ben szereplő csapatok bajnokságok szerint külön-külön csv fájllal rendelkezzenek.
 Ezeket a \teams mappában találhatod meg. Még üresek, de már tárolásra készek.
 Innentől jön a result táblázat és a folyamatos továbbgörgetett kalkulácció, amit majd az algoritmus a csapatok saját fájljába eltárol.
+
+
+Számítás: [(hazaiGól-vendégGól)-(H.PR-A.PR)-0,5]*025
+úJ PR: PRelőző+PRváltozó
+PR differencia: H.PR-A.PR
+
+Az azonos PR differenciáva lrendelkező meccsek kimenetelének százalékos arányosítása.
+
