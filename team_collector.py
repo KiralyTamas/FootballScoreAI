a95ed56f4088970c0d80900276155a792c016210
Király Tamás <tomaszpest@gmail.com>
2021-11-16 23:13:53 +0100

    Jó sokat haladtam, de még várat magára.

--- team_collector.py
+++ team_collector.py
@@ -1,23 +1,38 @@
 import pandas as pd
 import os
-cup=input("Melyik bajnokságról van szó?")
-folder=input("Hova legyenek elmentve a fájlok?")
-f = []
-for (dirpath, dirnames, filenames) in os.walk(os.path.abspath(folder)):
-    f.extend(filenames)
-    break
-
+import csv
+path=os.path.abspath("teams/bundesliga")
 
 def create_team_csv():
-    df = pd.read_csv(cup)
-    h_title = df["h_title"]
-    a_title = df["a_title"]
-    for i in range(len(h_title)):
-        if (h_title[i]+".csv") not in f:
-            with open(folder+"/"+h_title[i]+".csv", "w"):
+    cup = input("Melyik bajnokságról van szó?")
+    with open(cup, "r") as file:
+        csv_file = csv.reader(file)
+        df = pd.DataFrame(csv_file)
+        for row_index,row in df.iterrows():
+            if row_index==0:
                 continue
-        elif (a_title[i]+".csv") not in f:
-            with open(folder+"/"+a_title[i]+".csv", "w"):
-                continue
+            date=row[12],row[1]
+            h_team=row[3]
+            a_team=row[6]
+            h_gol = row[8]
+            a_gol = row[9]
+            score = int(h_gol)-int(a_gol)
+            data=date[0],date[1],h_team,h_gol,a_team,a_gol,score
+            if path+"\\"+h_team+".csv" == True:
+                with open(path+"\\"+h_team+".csv","r",newline='') as team_csv:
+                    csv_data=csv.reader(team_csv)
+                    for lines in csv_data:
+                        raw_data=[]
+                        raw_data.extend(lines)
+                    raw_data.insert(0,data)
+                with open(path+"\\"+h_team+".csv","a",newline='') as team_csv:
+                    team_file=csv.writer(team_csv)
+                    team_file.writerow(raw_data)
+            else:    
+                with open(path+"\\"+h_team+".csv","w",newline='') as team_csv:
+                    team_file=csv.writer(team_csv)
+                    team_file.writerow(data)
+            
+                
 
 create_team_csv()
---
+++ test.py
@@ -0,0 +1,4 @@
+number=[]
+for i in range(1,10,1):
+  number.insert(0,i)
+print(number)
