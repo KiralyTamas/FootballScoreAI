8ebb38f8ecbc0b3e96a9bcf713c23d71a50b9ec1
Király Tamás <tomaszpest@gmail.com>
2021-11-17 22:15:36 +0100

    Lényegében kész, már csak a PR számítást kell bevonni.

--- team_collector.py
+++ team_collector.py
@@ -1,38 +1,56 @@
 import pandas as pd
 import os
 import csv
-path=os.path.abspath("teams/bundesliga")
+path = os.path.abspath("teams/bundesliga")
+
 
 def create_team_csv():
     cup = input("Melyik bajnokságról van szó?")
     with open(cup, "r") as file:
         csv_file = csv.reader(file)
         df = pd.DataFrame(csv_file)
-        for row_index,row in df.iterrows():
-            if row_index==0:
+        for row_index, row in df.iterrows():
+            if row_index == 0:
                 continue
-            date=row[12],row[1]
-            h_team=row[3]
-            a_team=row[6]
-            h_gol = row[8]
-            a_gol = row[9]
-            score = int(h_gol)-int(a_gol)
-            data=date[0],date[1],h_team,h_gol,a_team,a_gol,score
-            if path+"\\"+h_team+".csv" == True:
-                with open(path+"\\"+h_team+".csv","r",newline='') as team_csv:
-                    csv_data=csv.reader(team_csv)
-                    for lines in csv_data:
-                        raw_data=[]
-                        raw_data.extend(lines)
-                    raw_data.insert(0,data)
-                with open(path+"\\"+h_team+".csv","a",newline='') as team_csv:
-                    team_file=csv.writer(team_csv)
-                    team_file.writerow(raw_data)
-            else:    
-                with open(path+"\\"+h_team+".csv","w",newline='') as team_csv:
-                    team_file=csv.writer(team_csv)
-                    team_file.writerow(data)
-            
-                
-
+            else:
+                math_id = row[0]
+                team_id = [row[2], row[5]]
+                teams = row[3], row[6]
+                gh = [row[8], row[9]]
+                xg = [row[10], row[11]]
+                date = row[12]
+                home_data = [date, math_id, team_id[0],teams[0], 
+                              team_id[1], teams[1], gh[0], gh[1],
+                              xg[0], xg[1]]
+                against_data=[date, math_id, team_id[1],teams[1], 
+                              team_id[0], teams[0], gh[1], gh[0],
+                              xg[1], xg[0]]
+                try:
+                    with open(path+"\\"+teams[0]+".csv","r",newline='',encoding="utf-8") as home_csv_old:
+                        home_table_old=csv.reader(home_csv_old)
+                        home_old_list=[]
+                        for home_list in home_table_old:
+                            home_old_list.append(home_list)
+                        with open(path+"\\"+teams[0]+".csv","a",newline='',encoding="utf-8") as home_csv:
+                            home_table=csv.writer(home_csv)
+                            if home_data not in home_old_list:
+                                home_table.writerow(home_data)
+                except FileNotFoundError:
+                    fx=open(path+"\\"+teams[0]+".csv","x")
+                    if FileNotFoundError ==True:
+                        continue
+                try:
+                    with open(path+"\\"+teams[1]+".csv","r",newline='',encoding="utf-8") as against_csv_old:
+                        against_table_old=csv.reader(against_csv_old)
+                        against_old_list=[]
+                        for against_list in against_table_old:
+                            against_old_list.append(against_list)
+                        with open(path+"\\"+teams[1]+".csv","a",newline='',encoding="utf-8") as against_csv:
+                            against_table=csv.writer(against_csv)
+                            if against_data not in against_old_list:
+                                against_table.writerow(against_data)
+                except FileNotFoundError:
+                    fx=open(path+"\\"+teams[1]+".csv","x")
+                    if FileNotFoundError:
+                        continue
 create_team_csv()
