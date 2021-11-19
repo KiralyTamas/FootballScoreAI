1e30e2218f25ae71e642eec7dc45f32da076f006
KiralyTamas <tomaszpest@gmail.com>
2021-11-19 13:28:09 +0100

    Tesztelem a fejlesztési lehetőségeket.

--- Test/test.py
+++ Test/test.py
@@ -1,25 +1,39 @@
 import csv
+import os
 list = [["1", "alma", "narancs", "dió", "kókusz"],
         ["2", "kocsi", "bicikli", "hajó", "repülő"],
         ["3", "nadrág", "kabát", "cipő", "sapka"],
         ["4", "fiú", "lány", "férfi", "nő"],
         ["5", "fekete", "fehér", "piros", "sárga"],
-        ["6","cápa","delfin","bálna","kardhal"],
-        ["7","NewYork","Tokyo","London","Texas"]]
-
+        ["6", "cápa", "delfin", "bálna", "kardhal"],
+        ["7", "NewYork", "Tokyo", "London", "Texas"]]
 
 
 def write():
-  try:
-    with open("proba.csv","r",encoding="utf-8", newline='') as file:
-      write_csv=csv.writer(file)
-      for item in reversed(list):
-        if item == list:
-          continue
-        else:
-          write_csv.writerow(item)
-  except OSError:
-    print("szar")
-    write()
+    try:
+        with open("proba.csv", "w", encoding="utf-8", newline='') as file:
+            write_csv = csv.writer(file)
+            for item in reversed(list):
+                item.insert(1, "univerzum")
+                if item == list:
+                    continue
+                else:
+                    write_csv.writerow(item)
+    except OSError:
+        print("szar")
+        write()
 
-write()
+
+def search():
+
+  f = []
+  g = []
+  h = []
+  for (dirpath, dirnames, filenames) in os.walk(input("Mi a konvertálandó fájlok mappájának útvonala: ")):
+    f.extend(filenames)
+    h.append(dirpath)
+  print(f)
+  print(g)
+  print(h)
+
+search()
