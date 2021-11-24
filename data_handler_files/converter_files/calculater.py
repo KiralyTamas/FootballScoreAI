41b6f1c6081d2d7a7bba55d8fe1e6a77aee51fbc
KiralyTamas <tomaszpest@gmail.com>
2021-11-24 14:43:44 +0100

    1 és 2 tizedesek

--- data_handler_files/converter_files/calculater.py
+++ data_handler_files/converter_files/calculater.py
@@ -1,102 +1,105 @@
+# Szükséges modulok meghívva
 import os
 import csv
 
+# A csapatok mappájának abszolúlt elérési útvonala
 final_path = os.path.abspath("teams")
 
-
+# Függvény kezdete
+# Az info tartalmazza az átküldött iterált csv_result sort, a team_header magáért beszél :)
 def calculate(info, team_header):
-    fix_num = 0.5, 0.25, 10
-    if os.path.exists(final_path+"\\"+info[3]+".csv") == False:
-        with open(final_path+"\\"+info[3]+".csv", "w", newline='', encoding="utf-8") as hdata:
-            header = csv.writer(hdata)
-            header.writerow(team_header)
-    if os.path.exists(final_path+"\\"+info[6]+".csv") == False:
-        with open(final_path+"\\"+info[6]+".csv", "w", newline='', encoding="utf-8") as adata:
-            header = csv.writer(adata)
-            header.writerow(team_header)
-    with open(final_path+"\\"+info[3]+".csv", "r", encoding="utf-8") as hdata:
-        data_hlist = csv.reader(hdata)
-        row_hlist = []
-        for hrow in data_hlist:
-            row_hlist.append(hrow)
-    with open(final_path+"\\"+info[6]+".csv", "r", encoding="utf-8") as adata:
-        data_alist = csv.reader(adata)
-        row_alist = []
-        for arow in data_alist:
-            row_alist.append(arow)
-    if len(row_hlist) and len(row_alist) == 1:
-      pr_changing = (
-            ((int(info[8])-int(info[9]))-(fix_num[2]-fix_num[2]))-fix_num[0])*fix_num[1]
-      xg_changing = (((float(info[10])-float(info[11])) -
-                        (fix_num[2]-fix_num[2]))-float(fix_num[0]))*float(fix_num[1])
-      prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(
-            fix_num[2]-fix_num[2]))-float(fix_num[0]))*float(fix_num[1])
-      new_hpr = fix_num[2]+float(pr_changing)
-      new_apr = fix_num[2]-float(pr_changing)
-      new_hxg = fix_num[2]+float(xg_changing)
-      new_axg = fix_num[2]-float(xg_changing)
-      new_hprxg = fix_num[2]+float(prxg_changing)
-      new_aprxg = fix_num[2]-float(prxg_changing)
-      datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,
-              fix_num[2], fix_num[2], fix_num[2], fix_num[2], fix_num[2], fix_num[2]]
-      return (datas)
-    elif len(row_hlist) == 1:
-      pr_changing = (
-                ((int(info[8])-int(info[9]))-(fix_num[2]-float(row_alist[-1][6])))-fix_num[0])*fix_num[1]
-      xg_changing = (
-                ((float(info[10])-float(info[11]))-(fix_num[2]-float(row_alist[-1][6])))-fix_num[0])*fix_num[1]
-      prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(
-                fix_num[2]-float(row_alist[-1][6])))-fix_num[0])*fix_num[1]
-      new_hpr = fix_num[2]+float(pr_changing)
-      old_apr = float(row_alist[-1][7])
-      new_apr = old_apr-float(pr_changing)
-      new_hxg = fix_num[2]+float(xg_changing)
-      old_axg = float(row_alist[-1][9])
-      new_axg = old_axg-float(xg_changing)
-      new_hprxg = fix_num[2]+float(prxg_changing)
-      old_aprxg = float(row_alist[-1][11])
-      new_aprxg = old_aprxg-float(prxg_changing)
-      datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,
-                fix_num[2], old_apr, fix_num[2], old_axg, fix_num[2], old_aprxg]
-      return (datas)
-    elif len(row_alist) == 1:
-      pr_changing = (
-                ((int(info[8])-int(info[9]))-(float(row_hlist[-1][6])-fix_num[2]))-fix_num[0])*fix_num[1]
-      xg_changing = (
-                ((float(info[10])-float(info[11]))-(float(row_hlist[-1][6])-fix_num[2]))-fix_num[0])*fix_num[1]
-      prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(
-                float(row_alist[-1][6])-fix_num[2]))-fix_num[0])*fix_num[1]
-      old_hpr = float(row_hlist[-1][7])
-      new_hpr = old_hpr+float(pr_changing)
-      new_apr = fix_num[2]-float(pr_changing)
-      old_hxg = float(row_hlist[-1][9])
-      new_hxg = old_hxg+float(xg_changing)
-      new_axg = fix_num[2]-float(xg_changing)
-      old_hprxg = float(row_hlist[-1][11])
-      new_hprxg = old_hprxg+float(prxg_changing)
-      new_aprxg = fix_num[2]-float(prxg_changing)
-      datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,
-                old_hpr, fix_num[2], old_hxg, fix_num[2], old_hprxg, fix_num[2]]
-      return (datas)
-    else:
-        pr_changing = ((int(info[8])-int(info[9]))-(float(row_hlist[-1]
-                      [6])-float(row_alist[-1][6]))-fix_num[0])*fix_num[1]
-        xg_changing = ((float(info[10])-float(info[11]))-(float(row_hlist[-1][6])-float(
-            row_alist[-1][6]))-fix_num[0])*fix_num[1]
-        prxg_changing = ((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(
-            float(row_hlist[-1][6])-float(row_alist[-1][6]))-fix_num[0])*fix_num[1]
-        old_hpr = float(row_hlist[-1][7])
-        new_hpr = old_hpr+float(pr_changing)
-        old_apr = float(row_alist[-1][7])
-        new_apr = old_apr-float(pr_changing)
-        old_hxg = float(row_hlist[-1][9])
-        new_hxg = old_hxg+float(xg_changing)
-        old_axg = float(row_alist[-1][9])
-        new_axg = old_axg-float(xg_changing)
-        old_hprxg = float(row_hlist[-1][11])
-        new_hprxg = old_hprxg+float(prxg_changing)
-        old_aprxg = float(row_alist[-1][11])
-        new_aprxg = old_aprxg-float(prxg_changing)
-        datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,
-                old_hpr, old_apr, old_hxg, old_axg, old_hprxg, old_aprxg]
-        return (datas)
+# Fix számok elmentése 0,1,2 indexxel hivatkozunk majd rájuk
+	fix_num = 0.5, 0.25, 10
+# Ha a info[3]:Hazai CSapat és info[6]:Vendég Csapat által visszaadott csapatnév alapján nincs csv fájl,
+# akkor létrehozza a fájlt és az áthozott team_header adatait beírja fejlécként.
+	if os.path.exists(final_path+"\\"+info[3]+".csv") == False:
+		with open(final_path+"\\"+info[3]+".csv", "w", newline='', encoding="utf-8") as hdata:
+			header = csv.writer(hdata, dialect='excel')
+			header.writerow(team_header)
+	if os.path.exists(final_path+"\\"+info[6]+".csv") == False:
+		with open(final_path+"\\"+info[6]+".csv", "w", newline='', encoding="utf-8") as adata:
+			header = csv.writer(adata, dialect='excel')
+			header.writerow(team_header)
+# Az info[3] és info[6] csapatok fájljait megnyitja, beolvassa és beiterálja a row_hlist és row_alist változókba.
+	with open(final_path+"\\"+info[3]+".csv", "r", encoding="utf-8") as hdata:
+		data_hlist = csv.reader(hdata)
+		row_hlist = []
+		for hrow in data_hlist:
+			row_hlist.append(hrow)
+	with open(final_path+"\\"+info[6]+".csv", "r", encoding="utf-8") as adata:
+		data_alist = csv.reader(adata)
+		row_alist = []
+		for arow in data_alist:
+			row_alist.append(arow)
+# Innentől kezdődik a tényleges számítás. "if":mindkét csapat csak a fejléccel rendelkezik.
+# Ez csak a 2014-es év elején fordul elő, mikor minden csapat egymással az első meccsüket játszák.
+# ekkor csaka az iterált sorból dolgozik és a kezdeti 10-es értéket a fix_num[2] bekéréssel számolja.
+	if len(row_hlist) and len(row_alist) == 1:
+		pr_changing = (((int(info[8])-int(info[9]))-(fix_num[2]-fix_num[2]))-fix_num[0])*fix_num[1]
+		xg_changing = (((float(info[10])-float(info[11])) -(fix_num[2]-fix_num[2]))-float(fix_num[0]))*float(fix_num[1])
+		prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(fix_num[2]-fix_num[2]))-float(fix_num[0]))*float(fix_num[1])
+# Itt az eredménnyel módosítjuk a "régi értéket", jelen esetben csak a 10-es kezdő értéket.
+		new_hpr = fix_num[2]+float(pr_changing)
+		new_apr = fix_num[2]-float(pr_changing)
+		new_hxg = fix_num[2]+float(xg_changing)
+		new_axg = fix_num[2]-float(xg_changing)
+		new_hprxg = fix_num[2]+float(prxg_changing)
+		new_aprxg = fix_num[2]-float(prxg_changing)
+# Itt előkészítjük az eredményeket egy "datas" változóba és a return paranccsal visszaadjuk
+# ezt a "datas" adatokat a team_collector fájlnak.
+		datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,fix_num[2], fix_num[2], fix_num[2], fix_num[2], fix_num[2], fix_num[2]]
+		return (datas)
+# Innentől a folyamat lényegében ugyanaz.
+# Az első "elif" csinálja a számítást, ha a hazai csapat érkezett újjonnan a bajnokságba
+# A második "elif" értelemszerüen ha a vendég csapat az új, vagyis nincs mit kiemelni a saját fájljából.
+# Az "else" csinálja a "régi" csapatok számítását. Ekkor ne mcsak az iterált csv_resultot használja
+# hanem a két csapat fájljaiból is ki tudja emelni az előző mérkőzésük változásait.
+# Természetesen csak az egyik feltétel fut le iterálásonként, mindegyik a maga "datas" adatait küldi vissza.
+	elif len(row_hlist) == 1:
+		pr_changing = (((int(info[8])-int(info[9]))-(fix_num[2]-float(row_alist[-1][6])))-fix_num[0])*fix_num[1]
+		xg_changing = (((float(info[10])-float(info[11]))-(fix_num[2]-float(row_alist[-1][6])))-fix_num[0])*fix_num[1]
+		prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(fix_num[2]-float(row_alist[-1][6])))-fix_num[0])*fix_num[1]
+		new_hpr = fix_num[2]+float(pr_changing)
+		old_apr = float(row_alist[-1][7])
+		new_apr = old_apr-float(pr_changing)
+		new_hxg = fix_num[2]+float(xg_changing)
+		old_axg = float(row_alist[-1][9])
+		new_axg = old_axg-float(xg_changing)
+		new_hprxg = fix_num[2]+float(prxg_changing)
+		old_aprxg = float(row_alist[-1][11])
+		new_aprxg = old_aprxg-float(prxg_changing)
+		datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,fix_num[2], old_apr, fix_num[2], old_axg, fix_num[2], old_aprxg]
+		return (datas)
+	elif len(row_alist) == 1:
+		pr_changing = (((int(info[8])-int(info[9]))-(float(row_hlist[-1][6])-fix_num[2]))-fix_num[0])*fix_num[1]
+		xg_changing = (((float(info[10])-float(info[11]))-(float(row_hlist[-1][6])-fix_num[2]))-fix_num[0])*fix_num[1]
+		prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(float(row_alist[-1][6])-fix_num[2]))-fix_num[0])*fix_num[1]
+		old_hpr = float(row_hlist[-1][7])
+		new_hpr = old_hpr+float(pr_changing)
+		new_apr = fix_num[2]-float(pr_changing)
+		old_hxg = float(row_hlist[-1][9])
+		new_hxg = old_hxg+float(xg_changing)
+		new_axg = fix_num[2]-float(xg_changing)
+		old_hprxg = float(row_hlist[-1][11])
+		new_hprxg = old_hprxg+float(prxg_changing)
+		new_aprxg = fix_num[2]-float(prxg_changing)
+		datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,old_hpr, fix_num[2], old_hxg, fix_num[2], old_hprxg, fix_num[2]]
+		return (datas)
+	else:
+			pr_changing = ((int(info[8])-int(info[9]))-(float(row_hlist[-1][6])-float(row_alist[-1][6]))-fix_num[0])*fix_num[1]
+			xg_changing = ((float(info[10])-float(info[11]))-(float(row_hlist[-1][6])-float(row_alist[-1][6]))-fix_num[0])*fix_num[1]
+			prxg_changing = ((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(float(row_hlist[-1][6])-float(row_alist[-1][6]))-fix_num[0])*fix_num[1]
+			old_hpr = float(row_hlist[-1][7])
+			new_hpr = old_hpr+float(pr_changing)
+			old_apr = float(row_alist[-1][7])
+			new_apr = old_apr-float(pr_changing)
+			old_hxg = float(row_hlist[-1][9])
+			new_hxg = old_hxg+float(xg_changing)
+			old_axg = float(row_alist[-1][9])
+			new_axg = old_axg-float(xg_changing)
+			old_hprxg = float(row_hlist[-1][11])
+			new_hprxg = old_hprxg+float(prxg_changing)
+			old_aprxg = float(row_alist[-1][11])
+			new_aprxg = old_aprxg-float(prxg_changing)
+			datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,old_hpr, old_apr, old_hxg, old_axg, old_hprxg, old_aprxg]
+			return (datas)
--- team_collector.py
+++ team_collector.py
@@ -1,101 +1,118 @@
+# Modulok meghívása
 import pandas as pd
 import os
 import csv
+# A kalkulátor fájl behívása
 from data_handler_files.converter_files.calculater import calculate as cal
 
+# A csapatok mappájának abszolúlt elérési útvonala
 final_path = os.path.abspath("teams")
-start_path=input("Mi a konvertálandó fájlok mappájának útvonala: ")
+# Itt kérdezi meg a terminálban az útvonalat, a csv_result mappát kell belehúzni a terminálba.
+start_path = input("Mi a számításokra használandó fájlok mappájának útvonala: ")
 
+# Függvény kezdete
 def create_team_csv():
-    result_header = ["Dátum", "Meccs-Id", "Hazai-Csapat", "Ellenfél-Csapat",
-                    "Hazai-Gól", "Ellenfél-Gól", "Hazai-XG", "Ellenfél-XG", 
-                    "Hazai-PR", "Ellenfél-PR","PR-diff","Hazai-xgPR","Ellenfél-xgPR",
-                    "Hazai-Mixed-PR","Ellenfél-Mixed-PR","H%","D%","A%","ForeCast-W",
-                    "ForeCast-D","ForeCast-A"]
-    team_header=["Dátum","Meccs-Id","Fő-Csapat","Ellenfél-Csapat","Hazai-Gól",
-                "Ellenfél-Gól","Meccs-Előtti-PR","Meccs-Utáni-PR","Meccs-Előtti-xgPR",
-                "Meccs-Utáni-xgPR","Meccs-Előtti-Mixed_PR","Meccs-Utáni-Mixed_PR"]
-    f = []
-    g = []
-    h = []
-    for (dirpath, dirnames, filenames) in os.walk(start_path):
-        f.extend(filenames)
-        g.extend(dirnames)
-        h.append(dirpath)
-    for path in h:
-        for file in f:
-            print(file)
-            try:
-                with open(path+"\\"+file, "r") as file:
-                    csv_file = csv.reader(file)
-                    df = pd.DataFrame(csv_file)
-                    for row_index, row in df.iterrows():
-                        if row_index == 0:
-                            if os.path.exists(os.path.abspath("main_result")+"\\main_result.csv")==False:
-                                with open(os.path.abspath("main_result")+"\\main_result.csv","w",newline='',encoding="utf-8")as main:
-                                            main_list=csv.writer(main)
-                                            main_list.writerow(result_header)                
-                        else:
-                            datas=cal(row,team_header)
-                            date = row[12]
-                            math_id = row[0]
-                            teams = row[3], row[6]
-                            score = [row[8], row[9]]
-                            xg = [row[10], row[11]]
-                            pr_diff=datas[6]-datas[7]
-                            pr_diff=("%.1f" % pr_diff)
-                            pr=""
-                            datas[0]=("%.4f" % datas[0])
-                            datas[1]=("%.4f" % datas[1])
-                            datas[2]=("%.4f" % datas[2])
-                            datas[3]=("%.4f" % datas[3])
-                            datas[4]=("%.4f" % datas[4])
-                            datas[5]=("%.4f" % datas[5])   
-                            forecast=[row[13],row[14],row[15]]
-                            main_result=[date,math_id,teams[0],teams[1],score[0],
-                                        score[1],xg[0],xg[1],datas[6],datas[7],pr_diff,datas[8],datas[9],datas[10],
-                                        datas[11],pr,pr,pr,forecast[0],forecast[1],forecast[2]]
-                            home_data = [date, math_id, teams[0],
-                                        teams[1], score[0], score[1],datas[6], datas[0],
-                                        datas[8],datas[2],datas[10],datas[4]]
-                            against_data = [date, math_id, teams[1],
-                                            teams[0], score[1], score[0], datas[7], datas[1],
-                                            datas[9],datas[3],datas[11],datas[5]]                            
-                            with open(os.path.abspath("main_result")+"\\main_result.csv","r") as read_main:
-                                read_main=csv.reader(read_main)
-                                list_main=[]
-                                for i in read_main:
-                                    list_main.append(i)
-                                if main_result in list_main:
-                                    continue
-                                else:
-                                    with open(os.path.abspath("main_result")+"\\main_result.csv","a",newline='',encoding="utf-8")as main:
-                                        main=csv.writer(main)
-                                        main.writerow(main_result)
-                            with open(final_path+"\\"+teams[0]+".csv", "r", newline='', encoding="utf-8") as home_csv_old:
-                                home_table_old = csv.reader(home_csv_old)
-                                home_old_list = []
-                                for home_list in home_table_old:
-                                    home_old_list.append(home_list)
-                                if [home_data] not in home_old_list:
-                                    with open(final_path+"\\"+teams[0]+".csv", "a", newline='', encoding="utf-8") as home_csv:
-                                        home_table = csv.writer(home_csv)
-                                        home_table.writerow(home_data)
-                            with open(final_path+"\\"+teams[1]+".csv", "r", newline='', encoding="utf-8") as against_csv_old:
-                                against_table_old = csv.reader(
-                                    against_csv_old)
-                                against_old_list = []
-                                for against_list in against_table_old:
-                                    against_old_list.append(
-                                        against_list)
-                                with open(final_path+"\\"+teams[1]+".csv", "a", newline='', encoding="utf-8") as against_csv:
-                                    against_table = csv.writer(against_csv)
-                                    if against_data not in against_old_list:
-                                        against_table.writerow(against_data)
-            except FileNotFoundError:
-                if FileNotFoundError == True:
-                    print("Szar")
+# A main_result és a csapat_csv-k fejlécének elnevezései
+  result_header = ["Dátum", "Meccs-Id", "Hazai-Csapat", "Ellenfél-Csapat",
+                  "Hazai-Gól", "Ellenfél-Gól", "Hazai-XG", "Ellenfél-XG",
+                  "Hazai-PR", "Ellenfél-PR", "PR-diff", "Hazai-xgPR", "Ellenfél-xgPR",
+                  "Hazai-Mixed-PR", "Ellenfél-Mixed-PR", "H%", "D%", "A%", "ForeCast-W",
+                  "ForeCast-D", "ForeCast-A"]
+  team_header = ["Dátum", "Meccs-Id", "Fő-Csapat", "Ellenfél-Csapat", "Hazai-Gól",
+                "Ellenfél-Gól", "Meccs-Előtti-PR", "Meccs-Utáni-PR", "Meccs-Előtti-xgPR",
+                "Meccs-Utáni-xgPR", "Meccs-Előtti-Mixed_PR", "Meccs-Utáni-Mixed_PR"]
+# Az os.walk iterál végig a csv_result mappa almappáin és az azokban lévő fájlokon.
+# A "h" tárolja a mappa útvonalakat, az "f" a mappákban lévő fájlok neveit
+  f = []
+  g = []
+  h = []
+  for (dirpath, dirnames, filenames) in os.walk(start_path):
+    f.extend(filenames)
+    g.extend(dirnames)
+    h.append(dirpath)
+# Az első "for" a mappákon iterál végig, a második "for" a mappákban lévő fájlokon.
+  for path in h:
+    for file in f:
+      print(file)
+      try:
+# Megnyitja a "for" által megadott aktuális csv_result fájlt, "df" változóba kilistázza, aztán végigiterál rajta.
+        with open(path+"\\"+file, "r") as file:
+          csv_file = csv.reader(file)
+          df = pd.DataFrame(csv_file)
+          for row_index, row in df.iterrows():
+# Minden fájlban van fejléc. Az új fájl 0-adik indexén a fejléc van.
+# Ha ennél a sornál jár az iteráció, megvizsgálja, hogy létezik-e a main_result csv fájl.
+# Ha létezik, akkor szimplán átugorja ezt az iterált sort. Ha nem, akkor létrehozza a fájlt és beírja
+# Az előzőleg a result_header változóba eltárolt fejlecet.
+            if row_index == 0:
+              if os.path.exists(os.path.abspath("main_result")+"\\main_result.csv") == False:
+                with open(os.path.abspath("main_result")+"\\main_result.csv", "w", newline='',encoding="utf-8") as main:
+                  main_list = csv.writer(main, dialect='excel')
+                  main_list.writerow(result_header)
+            else:
+# A cal(row, team_header) meghívásával átadjuk az aktuálisan iterált csv_result fájl sorát és a csapat_csv fejlécét.
+                datas = cal(row, team_header)
+# Visszaérkeznek a "calculater" által küldött "datas" adatok.
+# A csv_result 
+                date = row[12]
+                math_id = row[0]
+                teams = row[3], row[6]
+                score = [row[8], row[9]]
+                xg = [row[10], row[11]]
+                pr_diff = datas[6]-datas[7]
+                if pr_diff == -0 or -0.0:
+                  pr_diff=0
+                pr_diff = ("%.2f" % pr_diff)
+                pr = ""
+                datas[0] = ("%.4f" % datas[0])
+                datas[1] = ("%.4f" % datas[1])
+                datas[2] = ("%.4f" % datas[2])
+                datas[3] = ("%.4f" % datas[3])
+                datas[4] = ("%.4f" % datas[4])
+                datas[5] = ("%.4f" % datas[5])
+                forecast = [row[13], row[14], row[15]]
+                main_result = [date, math_id, teams[0], teams[1], score[0],
+                              score[1], xg[0], xg[1], datas[6], datas[7], pr_diff, datas[8], datas[9], datas[10],
+                              datas[11], pr, pr, pr, forecast[0], forecast[1], forecast[2]]
+                home_data = [date, math_id, teams[0],
+                            teams[1], score[0], score[1], datas[6], datas[0],
+                            datas[8], datas[2], datas[10], datas[4]]
+                against_data = [date, math_id, teams[1],
+                              teams[0], score[1], score[0], datas[7], datas[1],
+                              datas[9], datas[3], datas[11], datas[5]]
+                with open(os.path.abspath("main_result")+"\\main_result.csv", "r") as read_main:
+                  read_main = csv.reader(read_main)
+                  list_main = []
+                  for i in read_main:
+                    list_main.append(i)
+                  if main_result in list_main:
                     continue
-
+                  else:
+                    with open(os.path.abspath("main_result")+"\\main_result.csv", "a", newline='',encoding="utf-8")as main:
+                      main = csv.writer(main, dialect='excel')
+                      main.writerow(main_result)
+                with open(final_path+"\\"+teams[0]+".csv", "r", newline='', encoding="utf-8") as home_csv_old:
+                  home_table_old = csv.reader(home_csv_old)
+                  home_old_list = []
+                  for home_list in home_table_old:
+                    home_old_list.append(home_list)
+                  if [home_data] not in home_old_list:
+                    with open(final_path+"\\"+teams[0]+".csv", "a", newline='', encoding="utf-8") as home_csv:
+                      home_table = csv.writer(home_csv, dialect='excel')
+                      home_table.writerow(home_data)
+                with open(final_path+"\\"+teams[1]+".csv", "r", newline='', encoding="utf-8") as against_csv_old:
+                  against_table_old = csv.reader(against_csv_old)
+                  against_old_list = []
+                  for against_list in against_table_old:
+                    against_old_list.append(
+                    against_list)
+                with open(final_path+"\\"+teams[1]+".csv", "a", newline='', encoding="utf-8") as against_csv:
+                  against_table = csv.writer(against_csv, dialect='excel')
+                  if against_data not in against_old_list:
+                    against_table.writerow(against_data)
+      except FileNotFoundError:
+        if FileNotFoundError == True:
+          print("Szar")
+          continue
 
 create_team_csv()
