7150ff83ab8679bf961d5e685165a1c6f0ae030b
Király Tamás <tomaszpest@gmail.com>
2021-12-01 20:00:45 +0100

    Vendég PR beillesztve.

--- data_handler_files/converter_files/calculater.py
+++ data_handler_files/converter_files/calculater.py
@@ -38,50 +38,50 @@ def calculate(info, team_header): # Az info tartalmazza az átküldött iterált
 		datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,fix_num[2], fix_num[2], fix_num[2], fix_num[2], fix_num[2], fix_num[2]] # Itt előkészítjük az eredményeket egy "datas" változóba és a return paranccsal visszaadjuk
 		return (datas)           # ezt a "datas" adatokat a team_collector fájlnak.
 	elif len(row_hlist) == 1: # Innentől a folyamat lényegében ugyanaz.
-		pr_changing = (((int(info[8])-int(info[9]))-(fix_num[2]-float(row_alist[-1][8])))-fix_num[0])*fix_num[1]  # Az első "elif" csinálja a számítást, ha a hazai csapat érkezett újjonnan a bajnokságba
-		xg_changing = (((float(info[10])-float(info[11]))-(fix_num[2]-float(row_alist[-1][8])))-fix_num[0])*fix_num[1]   # A második "elif" értelemszerüen ha a vendég csapat az új, vagyis nincs mit kiemelni a saját fájljából.
-		prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(fix_num[2]-float(row_alist[-1][8])))-fix_num[0])*fix_num[1]   # Az "else" csinálja a "régi" csapatok számítását. Ekkor nem csak az iterált csv_resultot használja
+		pr_changing = (((int(info[8])-int(info[9]))-(fix_num[2]-float(row_alist[-1][9])))-fix_num[0])*fix_num[1]  # Az első "elif" csinálja a számítást, ha a hazai csapat érkezett újjonnan a bajnokságba
+		xg_changing = (((float(info[10])-float(info[11]))-(fix_num[2]-float(row_alist[-1][9])))-fix_num[0])*fix_num[1]   # A második "elif" értelemszerüen ha a vendég csapat az új, vagyis nincs mit kiemelni a saját fájljából.
+		prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(fix_num[2]-float(row_alist[-1][9])))-fix_num[0])*fix_num[1]   # Az "else" csinálja a "régi" csapatok számítását. Ekkor nem csak az iterált csv_resultot használja
 		new_hpr = fix_num[2]+float(pr_changing)   # hanem a két csapat fájljaiból is ki tudja emelni az előző mérkőzésük változásait.
-		old_apr = float(row_alist[-1][9])    # Természetesen csak az egyik feltétel fut le iterálásonként, mindegyik a maga "datas" adatait küldi vissza.
+		old_apr = float(row_alist[-1][10])    # Természetesen csak az egyik feltétel fut le iterálásonként, mindegyik a maga "datas" adatait küldi vissza.
 		new_apr = old_apr-float(pr_changing)
 		new_hxg = fix_num[2]+float(xg_changing)
-		old_axg = float(row_alist[-1][11])
+		old_axg = float(row_alist[-1][12])
 		new_axg = old_axg-float(xg_changing)
 		new_hprxg = fix_num[2]+float(prxg_changing)
-		old_aprxg = float(row_alist[-1][13])
+		old_aprxg = float(row_alist[-1][14])
 		new_aprxg = old_aprxg-float(prxg_changing)
 		datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,fix_num[2], old_apr, fix_num[2], old_axg, fix_num[2], old_aprxg]
 		return (datas)
 	elif len(row_alist) == 1:
-		pr_changing = (((int(info[8])-int(info[9]))-(float(row_hlist[-1][8])-fix_num[2]))-fix_num[0])*fix_num[1]
-		xg_changing = (((float(info[10])-float(info[11]))-(float(row_hlist[-1][8])-fix_num[2]))-fix_num[0])*fix_num[1]
-		prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(float(row_alist[-1][8])-fix_num[2]))-fix_num[0])*fix_num[1]
-		old_hpr = float(row_hlist[-1][9])
+		pr_changing = (((int(info[8])-int(info[9]))-(float(row_hlist[-1][9])-fix_num[2]))-fix_num[0])*fix_num[1]
+		xg_changing = (((float(info[10])-float(info[11]))-(float(row_hlist[-1][9])-fix_num[2]))-fix_num[0])*fix_num[1]
+		prxg_changing = (((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(float(row_alist[-1][9])-fix_num[2]))-fix_num[0])*fix_num[1]
+		old_hpr = float(row_hlist[-1][10])
 		new_hpr = old_hpr+float(pr_changing)
 		new_apr = fix_num[2]-float(pr_changing)
-		old_hxg = float(row_hlist[-1][11])
+		old_hxg = float(row_hlist[-1][12])
 		new_hxg = old_hxg+float(xg_changing)
 		new_axg = fix_num[2]-float(xg_changing)
-		old_hprxg = float(row_hlist[-1][13])
+		old_hprxg = float(row_hlist[-1][14])
 		new_hprxg = old_hprxg+float(prxg_changing)
 		new_aprxg = fix_num[2]-float(prxg_changing)
 		datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,old_hpr, fix_num[2], old_hxg, fix_num[2], old_hprxg, fix_num[2]]
 		return (datas)
 	else:
-			pr_changing = ((int(info[8])-int(info[9]))-(float(row_hlist[-1][8])-float(row_alist[-1][8]))-fix_num[0])*fix_num[1]
-			xg_changing = ((float(info[10])-float(info[11]))-(float(row_hlist[-1][8])-float(row_alist[-1][8]))-fix_num[0])*fix_num[1]
-			prxg_changing = ((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(float(row_hlist[-1][8])-float(row_alist[-1][8]))-fix_num[0])*fix_num[1]
-			old_hpr = float(row_hlist[-1][9])
+			pr_changing = ((int(info[8])-int(info[9]))-(float(row_hlist[-1][9])-float(row_alist[-1][9]))-fix_num[0])*fix_num[1]
+			xg_changing = ((float(info[10])-float(info[11]))-(float(row_hlist[-1][9])-float(row_alist[-1][9]))-fix_num[0])*fix_num[1]
+			prxg_changing = ((int(info[8])-int(info[9]))-(float(info[10])-float(info[11]))-(float(row_hlist[-1][9])-float(row_alist[-1][9]))-fix_num[0])*fix_num[1]
+			old_hpr = float(row_hlist[-1][10])
 			new_hpr = old_hpr+float(pr_changing)
-			old_apr = float(row_alist[-1][9])
+			old_apr = float(row_alist[-1][10])
 			new_apr = old_apr-float(pr_changing)
-			old_hxg = float(row_hlist[-1][11])
+			old_hxg = float(row_hlist[-1][12])
 			new_hxg = old_hxg+float(xg_changing)
-			old_axg = float(row_alist[-1][11])
+			old_axg = float(row_alist[-1][12])
 			new_axg = old_axg-float(xg_changing)
-			old_hprxg = float(row_hlist[-1][13])
+			old_hprxg = float(row_hlist[-1][14])
 			new_hprxg = old_hprxg+float(prxg_changing)
-			old_aprxg = float(row_alist[-1][13])
+			old_aprxg = float(row_alist[-1][14])
 			new_aprxg = old_aprxg-float(prxg_changing)
 			datas = [new_hpr, new_apr, new_hxg, new_axg, new_hprxg, new_aprxg,old_hpr, old_apr, old_hxg, old_axg, old_hprxg, old_aprxg]
 			return (datas)
