4902889c6e399fa6bccf1c249f6ae7b80863b8f5
KiralyTamas <tomaszpest@gmail.com>
2021-12-10 14:19:59 +0100

    Otthon folytatom

--- data_handler_files/converter_files/percentage_collector.py
+++ data_handler_files/converter_files/percentage_collector.py
@@ -4,6 +4,7 @@ import os
 
 def per_col():
     pr_diff = []
+    checking_list=[]
     percentage_home_list=[]
     percentage_deal_list=[]
     percentage_against_list=[]
@@ -16,8 +17,11 @@ def per_col():
     with open(os.path.abspath("..\..\converted_csv_datas\main_result")+"\\main_result.csv", "r") as file:
         file = csv.reader(file)
         for index, row in enumerate(file):
+            fragment=[]
             if index == 0:
                 continue
+            fragment=[row[10],row[18],row[19],row[20]]
+            checking_list.append(fragment)
             if float(row[10]) not in pr_diff:
                 pr_diff.append(float(row[10]))
     pr_diff = sorted(pr_diff, key=lambda line: line, reverse=False)
@@ -25,26 +29,10 @@ def per_col():
         os.remove(path+diff_home_table)
     with open(path+diff_home_table,"a",newline='', encoding='utf-8') as m_file:
         m_file=csv.writer(m_file)
-        with open(os.path.abspath("..\..\converted_csv_datas\main_result")+"\\main_result.csv", "r") as file:
-            file = csv.reader(file)
-            for num in pr_diff:
-                num_home=[]
-                num_deal=[]
-                num_against=[]
-                hit=0
-                for index, row in enumerate(file):
-                    if index == 0:
-                        continue
-                    if str(num) == str(row[10]):
-                        line_len+=1
-                        hit+=1
-                        if line_len<hit:
-                            line_len+=1
-                        num_home.append(row[18])
-                        num_deal.append(row[19])
-                        num_against.append(row[20])
-                print(num_home)    
-                m_file.writerow(num_home)
-    
+        num_home=[]
+        num_deal=[]
+        num_against=[]
+        hit=0        
+    print(checking_list)
 
 per_col()
