d796351a3189eaec8e9b24ad6c769c8a4ac58b87
KiralyTamas <tomaszpest@gmail.com>
2021-12-11 16:14:08 +0100

    Táblák készülnek.

--- data_handler_files/converter_files/main_fixture_pr.py
+++ data_handler_files/converter_files/main_fixture_pr.py
@@ -6,16 +6,14 @@ from datetime import date as tdate
 from datetime import datetime
 
 
-def fixture_write():
+def fixture_pr():
     fixture_header = ["Dátum", "Meccs-Id", "Hazai-Csapat", "Vendég-Csapat", "Hazai-PR", "Vendég-PR", "PR-diff",
-        "PR-diff-darabszám", "H%", "D%", "A%", "Több mint -3", "-3", "-2", "-1", "0", "1", "2", "3", "Több mint 3"]
+        "PR-diff-darabszám", "H%", "D%", "A%", "Több mint 3", "3", "2", "1", "0", "-1", "-2", "-3", "Több mint -3"]
     if os.path.exists(os.path.abspath("..\..\converted_csv_datas\main_fixture")) == True:
-        os.remove(os.path.abspath(
-            "..\..\converted_csv_datas\main_fixture\main_fixture.csv"))
-        os.rmdir(os.path.abspath("..\..\converted_csv_datas\main_fixture"))
-    if os.path.exists(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture.csv")) == False:
+        os.remove(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture_pr.csv"))
+    if os.path.exists(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture_pr.csv")) == False:
         os.mkdir(os.path.abspath("..\..\converted_csv_datas\main_fixture"))
-    with open(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture.csv"), "w", newline='', encoding='utf-8') as file:
+    with open(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture_pr.csv"), "w", newline='', encoding='utf-8') as file:
         new_file = csv.DictWriter(file, dialect='excel',fieldnames=fixture_header)
         new_file.writeheader()
     main_dir = str()
@@ -50,7 +48,7 @@ def fixture_write():
                 match_id=match[0]
                 home_team=match[3]
                 against_team=match[6]
-                with open(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture.csv"),"r") as file:
+                with open(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture_pr.csv"),"r") as file:
                     file=csv.reader(file)
                     fix_table=[]
                     for row in file:
@@ -99,11 +97,11 @@ def fixture_write():
                                 score_diff_3 = row[28]
                                 score_diff_more =row[29]
                                 fixture_row=[date,match_id,home_team,against_team,home_pr,against_pr,pr_diff,diff_count,
-                                            home_win,deal_win,against_win,score_diff__more,score_diff__3,score_diff__2,
-                                            score_diff__1,score_diff_0,score_diff_1,score_diff_2,score_diff_3,score_diff_more]
-                    with open(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture.csv"),"a",newline='',encoding="utf-8") as file:
+                                            home_win,deal_win,against_win,score_diff_more,score_diff_3,score_diff_2,
+                                            score_diff_1,score_diff_0,score_diff__1,score_diff__2,score_diff__3,score_diff__more]
+                    with open(os.path.abspath("..\..\converted_csv_datas\main_fixture\main_fixture_pr.csv"),"a",newline='',encoding="utf-8") as file:
                         file=csv.writer(file, dialect='excel')
                         file.writerow(fixture_row)
-    date_sorting("..\..\converted_csv_datas\main_fixture\\main_fixture.csv")
+    date_sorting("..\..\converted_csv_datas\main_fixture\\main_fixture_pr.csv")
                     
-fixture_write()
+fixture_pr()
